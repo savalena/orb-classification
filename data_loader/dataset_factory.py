a23f2366ca7d44ef08e88b2f956a528b4f649e27
Evgeny Kruzhkov <quetalasj@gmail.com>
2022-02-25 14:52:57 +0300

    fix dataset loosing argument

--- data_loader/dataset_factory.py
+++ data_loader/dataset_factory.py
@@ -7,6 +7,7 @@ class DatasetFactory:
                  root,
                  dataset,
                  labels_file,
+                 classification_threshold,
                  transform=DEFAULT_TRANSFORM,
                  batch_size=128,
                  shuffle=True,
@@ -19,11 +20,13 @@ class DatasetFactory:
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.num_workers = num_workers
+        self.classification_threshold = classification_threshold
 
     def make_data_loader(self):
         return DataLoader(ORBFeaturesDataset(self.root,
                                              self.dataset,
                                              self.labels_file,
+                                             classification_threshold=self.classification_threshold,
                                              transform=self.transform),
                           batch_size=self.batch_size,
                           shuffle=self.shuffle,
