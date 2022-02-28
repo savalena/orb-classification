cb155d99b358197dd536a206a32c2eb3d2c445ae
Evgeny Kruzhkov <quetalasj@gmail.com>
2022-02-28 20:51:17 +0300

    fix activations error

--- classifier/utils.py
+++ classifier/utils.py
@@ -2,8 +2,9 @@ import torch.nn as nn
 
 
 def get_fc_layers(in_size, hidden_sizes, out_size):
-    layers = [nn.Linear(in_size, hidden_sizes[0])]
+    layers = [nn.Linear(in_size, hidden_sizes[0]), nn.ReLU()]
     for s in hidden_sizes:
         layers.append(nn.Linear(s, s))
+        layers.append(nn.ReLU())
     layers.append((nn.Linear(hidden_sizes[-1], out_size)))
     return nn.Sequential(*layers)
